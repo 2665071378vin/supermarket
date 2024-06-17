be3c41bb118e3106494041f6353f562432af1d9b
vin <2665071378@qq.com>
2024-06-17 23:45:42 +0800

    FastJson导致启动失败 Receiver class com.alibaba.fastjson.support.jaxrs.FastJsonAutoDiscoverable does not define or inherit an implementation of the resolved method 'abstract void configure(jakarta.ws.rs.core.FeatureContext)' of interface org.glassfish.jersey.internal.spi.AutoDiscoverable.

--- market-parent/market-eureka/src/main/java/com/market/EurekaApplication.java
+++ market-parent/market-eureka/src/main/java/com/market/EurekaApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class,args);
+        SpringApplication.run(EurekaApplication.class);
         System.out.printf("da");
     }
 }
