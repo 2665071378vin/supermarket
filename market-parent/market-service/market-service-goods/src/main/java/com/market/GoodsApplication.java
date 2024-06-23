5981bee4e99c6291c4105a2520b177cee4069470
vin <2665071378@qq.com>
2024-06-24 00:13:20 +0800

    设置jdk版本，和开启Eureka服务器注册自己信息 问题，Eureka客户端启动失败仍然未解决

--- market-parent/market-service/market-service-goods/src/main/java/com/market/GoodsApplication.java
+++ market-parent/market-service/market-service-goods/src/main/java/com/market/GoodsApplication.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @description TODO
  */
 @SpringBootApplication
-@EnableDiscoveryClient()
+@EnableDiscoveryClient
 public class GoodsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GoodsApplication.class, args);
